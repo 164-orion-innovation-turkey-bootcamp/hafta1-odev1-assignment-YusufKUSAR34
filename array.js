8c3666e9d95a3d1f38b50b5fa1592b206e8ecb3d
Yusuf KUŞAR <96016754+YusufKUSAR34@users.noreply.github.com>
2022-02-22 18:50:14 +0300

    find index pairs

---
+++ array.js
@@ -0,0 +1,23 @@
+var start = performance.now();//measuring code execution speed
+
+let array = [2,7,11,15,1,8]; 
+let target = 9; 
+
+//Function that returns indexes of pairs whose sum is equal to the target
+function findIndexPairs(array, target) {
+	let numIndexs = []; // store the index pairs
+	for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[i] + array[j] === target) {
+				// Adds index numbers of pairs whose sum is equal to the target.
+				numIndexs.push([i, j]);
+			}
+		}
+	}
+	return numIndexs; // returns indexes 
+};
+
+console.log(findIndexPairs(array, target));
+
+var finish = performance.now();//measuring code execution speed
+console.log(`Code performance testing: ${start - finish} millisecond`);
